99196dc71d0505c8708e349f21d3fe000a0fb0f3
marty <riedlmarty@gmail.com>
2021-10-30 18:18:19 +0200

    learning

---
+++ reactest/src/Tweet.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Tweet() {
+    return (
+        <div>
+            <h1>kokot</h1>
+            <p>ahoj</p>
+        </div>
+
+    );
+}
+export default Tweet;
